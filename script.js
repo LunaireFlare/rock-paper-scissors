497f8557a6079342e6e1b47bafdc7314814e57c9
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2024-10-30 17:01:45 +0100

    build: add function to play 5 rounds
    Added:
      - playGame() function encapsulating previously created score variables
        and playRound() function.
      - for loop to recall playRound() five times.
      - winner declaration at the end of all five rounds based on players'
        final score.
    
    Moved previously global-scoped compChoice and humChoice variables to
    their respective functions, leading to the reorganizing of said
    functions by adding separate console.log() and return statements so as
    to not return entire strings.
    The return statements now fit the playRound() conditions of a single
    word, and the strings have been moved to a console.log() statement.
    
    Created new compSelection and humSelection variables recalling the main
    getComputerChoice() and getHumanChoice() functions.

--- script.js
+++ script.js
@@ -1,63 +1,84 @@
-// BOULDER, PARCHMENT, LONGSWORD GAME
-
-// players score variables
-let humanScore = 0;
-let computerScore = 0;
-
-let compChoice = Math.floor(Math.random() * 3);
-let humChoice = prompt("Choose your weapon!", "Choose wisely.").toLowerCase();
+// BOULDER, PARCHMENT, LONGSWORD GAME - CONSOLE-BASED EDITION
 
 // computer randomly chooses and returns one of the available values
 function getComputerChoice() {
+  let compChoice = Math.floor(Math.random() * 3);
   switch (compChoice) {
     case 0:
+      console.log("<Foe> Your opponent has chosen... Boulder.");
       return "Boulder";
       break;
     case 1:
+      console.log("<Foe> Your opponent has chosen... Codex.");
       return "Codex";
       break;
     case 2: 
+      console.log("<Foe> Your opponent has chosen... Longsword.");
       return "Longsword";
       break; 
   }
 } 
-console.log(getComputerChoice());
 
-// human player chooses one of the three values in input
+// human player chooses one of the three values in input (in the absence of value, prompts again)
 function getHumanChoice() {
+  let humChoice = prompt("Choose your weapon!", "Boulder  |  Codex  |  Longsword").toLowerCase();
   switch (humChoice) {
     case "boulder":
-      return "Literally crushing your enemies. Straightforward and efficient.";
+      console.log("<Hero> Literally crushing your enemies. Straightforward and efficient.");
+      return humChoice;
       break;
     case "codex": 
-      return "Knowledge is power, but you already knew that, right?";
+      console.log("<Hero> Knowledge is power, so, the more pages the better, right?");
+      return humChoice;
       break;
     case "longsword":
-      return "A truly noble weapon, fit for the finest (and tallest) knight!";
+      console.log("<Hero> A noble weapon, fit for the finest - and tallest - knight!");
+      return humChoice;
       break;
     default:
-      return "You cannot go into battle empty-handed!";
+      console.error("You cannot go into battle empty-handed!");
+      return getHumanChoice();
       break;
   }
 }
-console.log(getHumanChoice());
 
-// single-round logic
-function playRound(compChoice, humChoice) {
-  if (humChoice == "boulder" && compChoice == 2 ||
-    humChoice == "codex" && compChoice == 0 ||
-    humChoice == "longsword" && compChoice == 1) {
-      ++humanScore;     
-      return "You have triumphed over your foe!";
-  } else if (humChoice == "boulder" && compChoice == 1 ||
-    humChoice == "codex" && compChoice == 2 ||
-    humChoice == "longsword" && compChoice == 0) {
-      ++computerScore;     
-      return "Your enemy was stronger... This time."
+// function to play 5 rounds
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  // logic of a single round = elect winner based on input and increment score accordingly
+  function playRound(compSelection, humSelection) { 
+    if (humSelection == "boulder" && compSelection == "Longsword" ||
+        humSelection == "codex" && compSelection == "Boulder" ||
+        humSelection == "longsword" && compSelection == "Codex") {
+          ++humanScore;     
+          return "You have triumphed over your foe!";
+    } else if (humSelection == "boulder" && compSelection == "Codex" ||
+        humSelection == "codex" && compSelection == "Longsword" ||
+        humSelection == "longsword" && compSelection == "Boulder") {
+          ++computerScore;     
+          return "Your enemy was stronger... This time."
     } else {
-      return "No one has won the battle."
+          return "Alas, a tie! No points this round."
     } 
+  }
+
+  // for loop to recall playRound() 5 times
+  for (let round = 1; round <= 5; round++) {
+    let compSelection = getComputerChoice();
+    let humSelection = getHumanChoice();
+    console.log(playRound(compSelection, humSelection));
+    console.log(`You have won ${humanScore} rounds. Your opponent has won ${computerScore} rounds.`);
+  }
+
+  // winner declaration at the end of all 5 rounds
+  if (humanScore > computerScore) {
+    return "Congratulations! You showed your might and won this duel."
+  } else if (humanScore < computerScore) {
+    return "Your opponent proved stronger than you. Keep practicing."
+  } else {
+    return "Alas, a tie! The victory remains unclaimed."
+  }
 }
-console.log(playRound(compChoice, humChoice));
-console.log(humanScore);
-console.log(computerScore);
+console.log(playGame());
