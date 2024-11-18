2172a323b3e4b5c0e5edd7efc561df3bb4e5c329
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2024-11-18 12:29:46 +0100

    refactor: break down into smaller functions

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const weaponButtons = document.querySelectorAll(".weapon-button");
 const weaponText = document.querySelector(".weapon-text");
+const outputText = document.querySelector(".output-text");
 
 const boulderWeapon = document.querySelector(".boulder-button");
 const codexWeapon = document.querySelector(".codex-button");
 const longswordWeapon = document.querySelector(".longsword-button");
+const replayButton = document.querySelector(".replay-button");
 
-const boulderIcon = document.querySelector('.boulder-icon');
-const codexIcon = document.querySelector('codex-icon');
-const longswordIcon = document.querySelector('longsword-icon');
+const boulderIcon = document.querySelector(".boulder-icon");
+const codexIcon = document.querySelector(".codex-icon");
+const longswordIcon = document.querySelector(".longsword-icon");
+const opponentIcon = document.querySelector(".opponent-icon");
+
+let heroScore = 0;
+let opponentScore = 0;
+let round = 0;
 
 function getHeroChoice() {
   let heroChoice = "";
@@ -19,81 +26,87 @@ function getHeroChoice() {
         weaponText.textContent = "Literally crushing your enemies. Straightforward and efficient.";
        } else if (weapon.classList.contains("codex-button")) {
         heroChoice = "codex";
-        weaponText.innerText = "Knowledge is power, so, the more pages the better, right?";
+        weaponText.textContent = "Knowledge is power, so, the more pages the better, right?";
        } else {
         heroChoice = "longsword";
-        weaponText.innerText = "A noble weapon, fit for the finest - and tallest - of knights!";
+        weaponText.textContent = "A noble weapon, fit for the finest - and tallest - of knights!";
        };
       });
     });
   };
 
-  getHeroChoice();
+function getOpponentChoice() {
+  let opponentChoice = Math.floor(Math.random() * 3);
+  let opponentTest = document.querySelector(".opponent-test");
 
-
-// computer randomly chooses and returns one of the available values
-function getComputerChoice() {
-  let compChoice = Math.floor(Math.random() * 3);
-  let opponentIcon = document.querySelector(".opponent-icon")
-
-  switch (compChoice) {
+  switch (opponentChoice) {
     case 0:
-      opponentIcon.classList.add;
+      opponentIcon.classList.replace("opponent-icon", "boulder-icon");
+      opponentTest.textContent = "boulder";
+      console.log("boulder");
+      return "boulder";
       break;
     case 1:
-      return "Codex";
+      opponentIcon.classList.replace("opponent-icon", "codex-icon");
+      opponentTest.textContent = "codex";
+      console.log("codex");
+      return "codex";
       break;
     case 2: 
-      return "Longsword";
-      break; 
-  }
-} 
+      opponentIcon.classList.replace("opponent-icon", "longsword-icon");
+      opponentTest.textContent = "longsword";
+      console.log("longsword");
+      return "longsword";
+      break;
+  };
+};
 
-// function to play 5 rounds
-function playGame() {
-  let heroScore = 0;
-  let opponentScore = 0;
 
-  // logic of a single round = elect winner based on input and increment score accordingly
-  function playRound(compSelection, humSelection) { 
-    if (humSelection == "boulder" && compSelection == "Longsword" ||
-        humSelection == "codex" && compSelection == "Boulder" ||
-        humSelection == "longsword" && compSelection == "Codex") {
-          ++heroScore;     
-          return "You have triumphed over your foe!";
-    } else if (humSelection == "boulder" && compSelection == "Codex" ||
-        humSelection == "codex" && compSelection == "Longsword" ||
-        humSelection == "longsword" && compSelection == "Boulder") {
-          ++opponentScore;     
-          return "Your enemy was stronger... This time."
-    } else {
-          return "Alas, a tie! No points this round."
-    } 
-  }
-
-  // for loop to recall playRound() 5 times
-  for (let round = 1; round <= 5; round++) {
-    let compSelection = getComputerChoice();
-    let humSelection = getHumanChoice();
-    console.log(playRound(compSelection, humSelection));
-    console.log(`%cYou have won ${humanScore} rounds. Your opponent has won ${computerScore} rounds.`, "font-style:italic");
-    console.log("%cChoose your weapon for the next round.", "background-color: #575c55");
-  }
-
-  // winner declaration at the end of all 5 rounds
-  if (humanScore > computerScore) {
-    console.log("%cCongratulations! You showed your might and won this duel.", "background-color:darkgreen")
-  } else if (humanScore < computerScore) {
-    console.log("%cYour opponent proved stronger than you. Keep practicing.", "background-color:darkred")
+function playRound(heroSelection, opponentSelection) { 
+  if (heroSelection == "boulder" && opponentSelection == "longsword" ||
+      heroSelection == "codex" && opponentSelection == "boulder" ||
+      heroSelection == "longsword" && opponentSelection == "codex") {
+        ++heroScore;     
+        outputText.textContent = "You have triumphed over your foe!";
+  } else if (heroSelection == "boulder" && opponentSelection == "codex" ||
+      heroSelection == "codex" && opponentSelection == "longsword" ||
+      heroSelection == "longsword" && opponentSelection == "boulder") {
+        ++opponentScore;     
+        outputText.textContent = "Your enemy was stronger... This time."
   } else {
-    console.log("%cAlas, a tie! The victory remains unclaimed.","background-color:#575c55");  
-  }
+    outputText.textContent = "Alas, a tie! No points this round."
+  } 
 }
 
-// let heroButton = document.querySelector('.boulder-button').addEventListener('click', changeImage() {
-//    heroButton.classList.remove(boulderIcon);
-//    heroButton.classList.add(codexIcon);
-// })
+  // for loop to recall playRound() 5 times
+  for (round = 1; round <= 4; round++) {
+    let opponentSelection = getOpponentChoice();
+    let heroSelection = getHeroChoice();
+    const roundText = document.querySelector(".round-text");
+
+    // roundText.textContent = `You have won ${heroScore} rounds. Your opponent has won ${opponentScore} rounds.`;
+  }
 
 
-// playGame();
+// winner declaration at the end of all 5 rounds
+function finishGame() {
+  const duelResult = document.querySelector(".duel-result");
+  if (heroScore > opponentScore) {
+    duelResult.textContent = "Congratulations! You showed your might and won this duel.";
+  } else if (heroScore < opponentScore) {
+    duelResult.textContent = "Your opponent proved stronger than you. Keep practicing.";
+  } else {
+    duelResult.textContent = "Alas, a tie! The victory remains unclaimed.";  
+  }
+  replayButton.style.visibility = "visible";
+}
+
+function playGame() {
+
+  getHeroChoice();
+  getOpponentChoice();
+  playRound();
+  finishGame();
+}
+
+playGame();
